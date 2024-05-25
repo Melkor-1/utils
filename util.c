98d7e95651fe3f1a6ea406b01685520fd6facbe7
Melkor-1 <140725427+Melkor-1@users.noreply.github.com>
2024-05-25 12:04:53 +0000

    Add C23 attributes, C99's static keyword in function parameters, strchrnul, strcasecmp, and some tests.

--- util.c
+++ util.c
@@ -1,22 +1,24 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
-#include <unistd.h>
-#include <errno.h>
+
+#ifdef __GNUC__
+    #define HAVE_GNU_STRCHRNUL
+#endif /* __GNUC__ */
+
 #include <string.h>
 #include <stdarg.h>
 
-#include "attrib.h"
 #include "util.h"
 
-#define FAIL -1
-
-int vasprintf(char **restrict strp, const char *restrict fmt, va_list ap)
+int util_vasprintf(char **restrict strp, 
+                   const char fmt[restrict static 1],
+                   va_list ap)
 {
     va_list ap_copy;
 
     va_copy(ap_copy, ap);
-    int const nwritten = vsnprintf(0, 0, fmt, ap_copy);
+    const int nwritten = vsnprintf(nullptr, 0, fmt, ap_copy);
 
     va_end(ap_copy);
 
@@ -25,67 +27,119 @@ int vasprintf(char **restrict strp, const char *restrict fmt, va_list ap)
     }
 
     *strp = malloc((size_t) nwritten + 1);
-    if (!(*strp)) {
+    if (*strp == nullptr) {
         goto fatal;
     }
 
-    int const ret_val = vsprintf(*strp, fmt, ap);
+    const int status = vsprintf(*strp, fmt, ap);
 
-    if (ret_val < 0) {
+    if (status < 0) {
         free(*strp);
         goto fatal;
     }
 
-    return ret_val;
+    return status;
 
   fatal:
-    *strp = 0;                  /* The BSD implementation set *strp to NULL on failure. */
-    return FAIL;
+    /* The BSD implementation sets *strp to nullptr on failure. Linux's leaves
+     * the contents undefined. Neither C nor POSIX has standarized this
+     * function as of yet. */
+    *strp = nullptr;
+    return -1;
 }
 
-ATTRIBUTE_PRINTF_LIKE(2, 3)
-int asprintf(char **restrict strp, const char *restrict fmt, ...)
+int util_asprintf(char **restrict strp, const char fmt[static 1], ...)
 {
     va_list argp;
 
     va_start(argp, fmt);
-    *strp = 0;
-    int const nwritten = kilo_vasprintf(strp, fmt, argp);
+    *strp = nullptr;
+    int nwritten = util_vasprintf(strp, fmt, argp);
 
     va_end(argp);
-
     return nwritten;
 }
 
-char *strdup(const char *s)
-{
-    const size_t len = strlen(s);
-
-    /* This could instead use asprintf(). */
-    void *new = malloc(len + 1);
-
-    if (!new) {
-        goto fatal;
-    }
-    return memcpy(new, s, len + 1);
-
-  fatal:
-    return 0;
-}
-
-char *stpcpy(char *restrict dest, const char *restrict src)
+char *util_stpcpy(char dest[restrict static 1],
+                  const char src[restrict static 1])
 {
     const size_t len = strlen(src);
 
-    /* The cast is required because pointer arithmetic on a void * 
-     * is not a valid operation. 
-     */
     return (char *) memcpy(dest, src, len + 1) + len;
 }
 
-char *basename(char *path)
-{
-    char *cp = strrchr(path, '/');
+#define gen_basename(version, ret_type, arg_type) \
+    ret_type util_basename_##version(arg_type path[static 1]) { \
+        char *const cp = strrchr(path, '/'); \
+        return cp ? cp + 1 : path; \
+    } 
 
-    return cp ? strdup(cp + 1) : strdup(path);
+gen_basename(vanilla, char *, char)
+gen_basename(const, const char *, const char)
+
+#define gen_strchrnul(version, ret_type, arg_type) \
+    ret_type util_strchrnul_##version(arg_type s[static 1], int c) { \
+        while (*s && *s != c) { ++s; } \
+        return s; \
+    }
+
+gen_strchrnul(vanilla, char *, char)
+gen_strchrnul(const, const char *, const char)
+
+int util_strcasecmp(const char s[restrict static 1], 
+                    const char t[restrict static 1])
+{
+    const unsigned char *us = (const unsigned char *)s;
+    const unsigned char *ut = (const unsigned char *)t;
+    unsigned int p, q;
+
+    do {
+        p = tolower(*us++);
+        q = tolower(*ut++);
+    } while (p == q && q != '\0');
+
+    return (p > q) - (p < q);
 }
+
+#ifdef TEST_MAIN
+
+#include <assert.h>
+
+int main(void)
+{
+    assert(strcmp(util_basename("/usr/lib"), "lib") == 0);
+    assert(strcmp(util_basename("/usr/"), "") == 0);
+    assert(strcmp(util_basename("usr"), "usr") == 0);
+    assert(strcmp(util_basename("/"), "") == 0);
+    assert(strcmp(util_basename("."), ".") == 0);
+    assert(strcmp(util_basename(".."), "..") == 0);
+
+    assert(util_strcasecmp("aPplE", "APPLE") == 0);
+    assert(util_strcasecmp("apple", "apple") == 0);
+    assert(util_strcasecmp("HELLO", "HELLO") == 0);
+    assert(util_strcasecmp("", "") == 0);
+    assert(util_strcasecmp("HrLLO", "HELLO"));
+
+    char *formatted = {};
+    int len = util_asprintf(&formatted, "%d", 10);
+    assert(len == 2);
+    assert(formatted);
+    assert(strcmp(formatted, "10") == 0);
+    free(formatted);
+    
+    static constexpr char haystack[] = "hello there";
+    assert(util_strchrnul(haystack, 'o') == strchr(haystack, 'o'));
+    assert(*util_strchrnul(haystack, 'p') == '\0');
+
+#ifdef HAVE_GNU_STRCHRNUL
+    /* Specify the prototype, because defining _GNU_SOURCE doesn't seem to. */
+    char *strchrnul(const char *p, int ch);
+
+    static constexpr char s[] = "hair"; 
+    static constexpr char t[] = "gown"; 
+    assert(util_strchrnul(s, 'h') == strchrnul(s, 'h'));
+    assert(util_strchrnul(t, 'g') == strchrnul(t, 'g'));
+#endif /* HAVE_GNU_STRCHRNUL */
+}
+
+#endif
--- util.h
+++ util.h
@@ -2,13 +2,41 @@
 #define UTIL_H 1
 
 #include <stdarg.h>
-#include "attrib.h"
 
-ATTRIBUTE_PRINTF_LIKE(2, 3)
-extern int asprintf(char **restrict strp, const char *restrict fmt, ...);
-int vasprintf(char **restrict strp, const char *restrict fmt, va_list ap);
-char *strdup(const char *s);
-char *stpcpy(char *restrict dest, const char *restrict src);
-char *basename(char *path);
+int util_vasprintf(char **restrict strp,
+                   const char fmt[restrict static 1], 
+                   va_list ap);
+
+[[gnu::format(printf, 2, 3)]] int util_asprintf(char **restrict strp,
+                                                const char fmt[restrict static 1], 
+                                                ...); 
+
+[[gnu::returns_nonnull]] char *util_stpcpy(char dest[restrict static 1],
+                                           const char src[restrict static 1]);
+
+[[gnu::returns_nonnull]] char *util_basename_vanilla(char path[static 1]);
+
+[[gnu::returns_nonnull]] const char *util_basename_const(const char path[static 1]);
+
+#define util_basename(s)                         \
+    _Generic((s),                                \
+            char *:       util_basename_vanilla, \
+            const char *: util_basename_const)(s)
+
+[[gnu::pure, gnu::returns_nonnull]] char *util_strchrnul_vanilla(
+        char s[static 1], 
+        int c);
+
+[[gnu::pure, gnu::returns_nonnull]] const char *util_strchrnul_const(
+        const char s[static 1], 
+        int c);
+
+#define util_strchrnul(s, c)                      \
+    _Generic((s),                                 \
+            char *:       util_strchrnul_vanilla, \
+            const char *: util_strchrnul_const)(s, c)
+
+[[gnu::pure]] int util_strcasecmp(const char s[restrict static 1], 
+                                  const char t[restrict static 1]);
 
 #endif                          /* UTIL_H */
