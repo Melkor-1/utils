be73ef78b539e9320e92c08f8a7545beb6f36628
Melkor-1 <140725427+Melkor-1@users.noreply.github.com>
2024-05-25 12:11:29 +0000

    Wrappers around write() and read() that retry on EINTR.

---
+++ io.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "io.h"
+
+ssize_t read_eintr(int fd, void *buf, size_t size)
+{
+    ssize_t ret = 0;
+
+    do {
+        errno = 0;
+        ret = read(fd, buf, size);
+    } while (ret == -1 && errno == EINTR);
+
+    return ret;
+}
+
+ssize_t write_eintr(int fd, const void *buf, size_t size)
+{
+    ssize_t ret = 0;
+
+    do {
+        errno = 0;
+        ret = write(fd, buf, size);
+    } while (ret == -1 && errno == EINTR);
+
+    return ret;
+}
+
+ssize_t write_all(int fd, 
+                  void *restrict buf, 
+                  size_t size, 
+                  size_t *restrict nwritten)
+{
+    size_t total_written = 0;
+    size_t bytes_left = size;
+    ssize_t ret;
+
+    for (ret = 0; total_written < size && ret != -1;
+        total_written += (size_t) ret) {
+        ret = write_eintr(fd, buf + total_written, bytes_left);
+        bytes_left -= (size_t) ret;
+    }
+    *nwritten = total_written;
+    return ret;
+}
---
+++ io.h
@@ -0,0 +1,25 @@
+#ifndef IO_H
+#define IO_H 1
+
+#include <sys/types.>
+
+/* These functions return -1 on failure, or a non-negative integer otherwise. */
+
+/* Version of read() that retries when interrupted by EINTR (possibly 
+ * by a SIGWINCH).  */
+[[nodiscard, gnu::nonnull]] ssize_t read_eintr(int fd, void *buf, size_t size);
+
+/* Version of write() that retries when interrupted by EINTR (possibly 
+ * by a SIGWINCH). */
+[[nodiscard, gnu::nonnull]] ssize_t write_eintr(int fd, 
+                                                const void *buf, 
+                                                size_t size);
+
+/* A wrapper around write_eintr() that's called in a loop until size bytes have 
+ * been written. Sets *nwritten to the number of total bytes written. */
+[[nodiscard, gnu::nonnull]] ssize_t write_all(int fd, 
+                                              void *restrict buf, 
+                                              size_t size, 
+                                              size_t *restrict nwritten);
+
+#endif /* IO_H */
